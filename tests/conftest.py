f1747bebf202f4ec26e62d7918e2ad341881a801
Dave Redmin <david.redmin@beta.dhs.gov>
2019-07-30 17:25:44 -0400

    Add some tests to validate composition

---
+++ tests/conftest.py
@@ -0,0 +1,44 @@
+"""pytest plugin configuration.
+
+https://docs.pytest.org/en/latest/writing_plugins.html#conftest-py-plugins
+"""
+import pytest
+
+
+@pytest.fixture(scope="session")
+def guacamole_container(dockerc):
+    """Return the guacamole container from the docker composition."""
+    # find the container by name even if it is stopped already
+    return dockerc.containers(service_names=["guacamole"], stopped=True)[0]
+
+
+@pytest.fixture(scope="session")
+def guacd_container(dockerc):
+    """Return the guacd container from the docker composition."""
+    # find the container by name even if it is stopped already
+    return dockerc.containers(service_names=["guacd"], stopped=True)[0]
+
+
+@pytest.fixture(scope="session")
+def postgres_container(dockerc):
+    """Return the postgres container from the docker composition."""
+    # find the container by name even if it is stopped already
+    return dockerc.containers(service_names=["postgres"], stopped=True)[0]
+
+
+def pytest_addoption(parser):
+    """Add new commandline options to pytest."""
+    parser.addoption(
+        "--runslow", action="store_true", default=False, help="run slow tests"
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    """Modify collected tests based on custom marks and commandline options."""
+    if config.getoption("--runslow"):
+        # --runslow given in cli: do not skip slow tests
+        return
+    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+    for item in items:
+        if "slow" in item.keywords:
+            item.add_marker(skip_slow)
---
+++ tests/container_test.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env pytest -vs
+"""Tests for Docker composition."""
+
+import os
+import pytest
+import time
+
+READY_MESSAGES = {
+    "guacamole": "Server startup in",
+    "guacd": "Listening on host 0.0.0.0",
+    "postgres": "database system is ready to accept connections",
+}
+
+
+def test_container_count(dockerc):
+    """Verify the test composition and container."""
+    # stopped parameter allows non-running containers in results
+    assert (
+        len(dockerc.containers(stopped=True)) == 5
+    ), "Wrong number of containers were started."
+
+
+def test_wait_for_ready_guacamole(guacamole_container):
+    """Wait for guacamole container to be ready."""
+    TIMEOUT = 10
+    ready_message = READY_MESSAGES["guacamole"]
+    for i in range(TIMEOUT):
+        if ready_message in guacamole_container.logs().decode("utf-8"):
+            break
+        time.sleep(1)
+    else:
+        raise Exception(
+            f"Container does not seem ready.  "
+            f'Expected "{ready_message}" in the log within {TIMEOUT} seconds.'
+        )
+
+
+def test_wait_for_ready_guacd(guacd_container):
+    """Wait for guacd container to be ready."""
+    TIMEOUT = 10
+    ready_message = READY_MESSAGES["guacd"]
+    for i in range(TIMEOUT):
+        if ready_message in guacd_container.logs().decode("utf-8"):
+            break
+        time.sleep(1)
+    else:
+        raise Exception(
+            f"Container does not seem ready.  "
+            f'Expected "{ready_message}" in the log within {TIMEOUT} seconds.'
+        )
+
+
+def test_wait_for_ready_postgres(postgres_container):
+    """Wait for postgres container to be ready."""
+    TIMEOUT = 10
+    ready_message = READY_MESSAGES["postgres"]
+    for i in range(TIMEOUT):
+        if ready_message in postgres_container.logs().decode("utf-8"):
+            break
+        time.sleep(1)
+    else:
+        raise Exception(
+            f"Container does not seem ready.  "
+            f'Expected "{ready_message}" in the log within {TIMEOUT} seconds.'
+        )
