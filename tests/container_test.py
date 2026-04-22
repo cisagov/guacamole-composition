16952f4f29ab1f9cfc013326cb6460e6c414899f
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-04-22 11:19:21 -0400

    Get rid of the init-guac-db service and instead use a shared volume
    This technique is described here:
    https://guacamole.apache.org/doc/gug/postgresql-auth.html#initializing-the-database

--- tests/container_test.py
+++ tests/container_test.py
@@ -14,7 +14,7 @@ def test_container_count(dockerc):
     """Verify the test composition and container."""
     # all parameter allows non-running containers in results
     assert (
-        len(dockerc.compose.ps(all=True)) == 5
+        len(dockerc.compose.ps(all=True)) == 4
     ), "Wrong number of containers were started."
 
 
